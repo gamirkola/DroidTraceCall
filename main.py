235155edd5a0c57e9f6ebcec7ebb96a0f22f79c8
gamirkola <mirkogal@gmail.com>
2021-01-12 23:52:25 +0100

    initial commit, only pyfiglet

---
+++ main.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+
+import os
+from pyfiglet import figlet_format
+
+
+if __name__ == '__main__':
+    f = figlet_format('D r o i d  T r a c e  C a l l', font='slant')
+    print(f)
