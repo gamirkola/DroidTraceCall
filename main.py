d80890f91b50d742330576eefdbf4fe07dee91af
gamirkola <mirkogal@gmail.com>
2021-01-14 11:25:25 +0100

    fixed strace compile script starting and push

--- main.py
+++ main.py
@@ -2,9 +2,8 @@
 
 import os
 from pyfiglet import figlet_format
-from subprocess import call
-strace_repo = 'https://github.com/strace/strace'
-strace_dir = 'tools/strace/strace_repo/'
+import subprocess
+
 
 if __name__ == '__main__':
 
@@ -12,16 +11,22 @@ if __name__ == '__main__':
     f = figlet_format('D r o i d  T r a c e  C a l l', font='slant')
     print(f)
 
+    see_connected_devices = input("[+] Do you want to verify if there are device connected? (y/n): ")
+    if see_connected_devices == 'y':
+        os.system('adb devices')
+        print("[!] Please select \"Always allow from this computer\" in the adb dialog!")
 
     strace_compile = input("[+] Do you want to compile the strace executable? (y/n): ")
     if strace_compile == 'y':
         #launch strace compile script
-        rc = call("./scripts/strace/compile_strace_aarch64.sh", shell=True)
+        subprocess.run('./compile_strace_aarch64.sh', shell=True, cwd='./scripts/strace/')
+
+    strace_push = input("[+] Do you want to push the strace executable to the phone? (y/n): ")
+    if strace_push == 'y':
+        #launch strace compile script
+        print('[*] Gaining root permissions...')
+        os.system('adb root')
+        print('[*] Pushing strace to /data/DroidTraceCall/')
+        os.system('adb push ./tools/strace/strace /data/DroidTraceCall/strace')
 
 
-
-
-
-    see_connected_devices = input("[+] Do you want to verify if there are device connected? (y/n): ")
-    if see_connected_devices == 'y':
-        os.system('adb devices')
